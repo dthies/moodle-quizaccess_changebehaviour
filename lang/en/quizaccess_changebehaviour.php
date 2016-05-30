08009cfa33321d523d651833254100a1ba85a988
Daniel Thies <dthies@ccal.edu>
2016-05-30 08:00:51 -0500

    Initial version

---
+++ lang/en/quizaccess_changebehaviour.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for the quizaccess_changebehaviour plugin.
+ *
+ * @package   quizaccess_changebehaviour
+ * @copyright 2016 Daniel Thies <dthies@ccal.edu>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$string['pluginname'] = 'Change question behaviour';
+$string['behaviourtime'] = 'Change question behaviour';
+$string['newbehaviour'] = 'New behaviour';
+$string['changebehaviournotice'] = 'The question behaviour will change after {$a->time}';
+$string['behaviourtime_help'] = 'Set a time after which all new attempts will have new question behaviour applied';
---
+++ rule.php
@@ -0,0 +1,98 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Implementaton of the quizaccess_changebehaviour plugin.
+ *
+ * @package    quizaccess
+ * @subpackage changebehaviour
+ * @copyright  2016 Daniel Thies <dthies@ccal.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/quiz/accessrule/accessrulebase.php');
+
+
+/**
+ * A rule representing to change the question behaviour setting after a time.
+ *
+ * @copyright  2016 Daniel Thies <dthies@ccal.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class quizaccess_changebehaviour extends quiz_access_rule_base {
+    protected $timeremaining;
+
+    public static function make(quiz $quizobj, $timenow, $canignoretimelimits) {
+
+        if (!empty($quizobj->get_quiz()->behaviourtime)) {
+            if ($quizobj->get_quiz()->behaviourtime < $timenow) {
+                $quizobj->get_quiz()->preferredbehaviour = $quizobj->get_quiz()->newbehaviour;
+            }
+        }
+
+        return new self($quizobj, $timenow);
+    }
+
+    public function description() {
+        if (!empty($this->quiz->behaviourtime)) {
+            $timeremaining = $this->quiz->behaviourtime - $this->timenow;
+            if ($timeremaining > 0) {
+                return get_string('changebehaviournotice', 'quizaccess_changebehaviour', array('time' => format_time($timeremaining)));
+            }
+        }
+        return '';
+    }
+
+    public static function add_settings_form_fields(
+            mod_quiz_mod_form $quizform, MoodleQuickForm $mform) {
+        $mform->addElement('date_time_selector', 'behaviourtime', get_string('behaviourtime', 'quizaccess_changebehaviour'), array('optional' => true, 'step' => 1));
+
+        $currentbehaviour = 'adaptive';
+        $behaviours = question_engine::get_behaviour_options($currentbehaviour);
+        $mform->addElement('select', 'newbehaviour',
+                get_string('newbehaviour', 'quizaccess_changebehaviour'), $behaviours);
+        $mform->addHelpButton('behaviourtime', 'behaviourtime', 'quizaccess_changebehaviour');
+
+    }
+
+    public static function save_settings($quiz) {
+        global $DB;
+
+        $DB->delete_records('quizaccess_changebehaviour', array('quizid' => $quiz->id));
+        if (!empty($quiz->behaviourtime)) {
+            $record = new stdClass();
+            $record->quizid = $quiz->id;
+            $record->behaviourtime = $quiz->behaviourtime;
+            $record->newbehaviour = $quiz->newbehaviour;
+            $DB->insert_record('quizaccess_changebehaviour', $record);
+        }
+    }
+
+    public static function delete_settings($quiz) {
+        global $DB;
+        $DB->delete_records('quizaccess_changebehaviour', array('quizid' => $quiz->id));
+    }
+
+    public static function get_settings_sql($quizid) {
+        return array(
+            'behaviourtime, newbehaviour',
+            'LEFT JOIN {quizaccess_changebehaviour} changebehaviour ON changebehaviour.quizid = quiz.id',
+            array());
+    }
+}
